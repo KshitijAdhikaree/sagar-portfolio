e35ec9c61ca96fd7a6b15c1e5e949822d08de2ed
KshitijAdhikaree <adhikareekshitij@gmail.com>
2023-06-12 00:30:19 +0545

    logo update with svg

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,19 +1,15 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { LogoIcon } from "./Icons";
 
 const Logo = () => {
   return (
-    <div className=' '>
-      <Link
-        href='/'
-        className='w-16 h-16 bg-dark text-light flex items-center justify-center border-solid rounded-full hover:bg-light
-        border-[2px] hover:border-dark  text-2xl font-bold dark:border-light dark:hover:bg-light hover:text-dark
-        md:text-xl'
-      >
-        S.A
+    <div>
+      <Link href="/">
+        <LogoIcon className="h-14 w-16 fill-light hover:fill-dark items-center dark:fill-light hover:scale-110" />
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
