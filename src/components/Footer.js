bb7d842237bf6780eff164b4a91bfd2cfff37204
Kshitij Adhikaree <48464685+KshitijAdhikaree@users.noreply.github.com>
2023-06-17 16:38:25 +0545

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:border-light dark:text-light sm:text-base'>
       <Layout className='py-8 flex flex-col items-center justify-between text-center lg:flex-col lg:py-6 '>
-        <span>Designed And Developed By:</span>
+        <span className ='text-sm'>Designed And Developed By:</span>
 
         <Link
           href='https://kshitijadhikaree.com.np'
