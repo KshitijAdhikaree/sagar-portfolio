6a97bbb464d78c0de9428451374523a329885111
Sagar <70172204+sagara92@users.noreply.github.com>
2026-01-30 13:23:13 -0500

    Add machine learning projects and their relevant data (hangman and blazar).

--- public/projectData.js
+++ public/projectData.js
@@ -2,13 +2,29 @@ export const ProjectData = [
   {
     id: 1,
     title:
+    "ML Classifications of Fermi-LAT Blazars",
+    description:
+    "The raw data from the Fermi-LAT telescope is analyzed for classification of BLL and FSRQ types of blazars. Three classifier ML algorithms were trained for it, Decision Tree (DT), XGBoost DT (GBDT), and Random Forest (RF). The GBDT classifier was found to be the most accurate with accuracy >90%.",
+    link: "/4FGL_Blazar_Classification_Project.ipynb",
+  },
+  {
+    id: 2,
+    title:
+      "Hangman ML Solver",
+    description:
+      "This project implements a N-Gram model to learn the word pattern from a dictionary to solve the Hangman Word Game. The N-Gram model is currently ~66% accurate when tested with training dictionary.",
+    link: "https://github.com/sagara92/Hangman_ML/blob/main/hangman_solver_ML_NGram.ipynb",
+  },
+  {
+    id: 3,
+    title:
       "Enhancing Periodicity Analysis Accuracy Through Phase Fold Amplitude Minimization (PFAM) Technique",
     description:
       "In time-series astronomy, periodicity study is one of the most useful tools to understand an astrophysical system. Research papers on periodicity study often show phase-folded plots to emphasize the presence of periodicity. Depending on the quality of the data points, we can use the phase-folded light curves (LCs) to further enhance the accuracy of the observed period. I discuss an amplitude minimization of the phase-folded LC, which can enhance the accuracy of observed periods in astrophysical LCs.",
     link: "/PFAM_technique.pdf",
   },
   {
-    id: 2,
+    id: 4,
     title:
       "Can Blazar flares in gamma-ray LCs be explained by jet angle and geometry?",
     description: "In Preparation.",
