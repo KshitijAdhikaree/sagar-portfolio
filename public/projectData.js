3b211f9edfb9c32260b29980ac1486aa17b6ecb0
Sagar <70172204+sagara92@users.noreply.github.com>
2026-02-04 11:52:47 -0500

    Add a image transfer ML project.

--- public/projectData.js
+++ public/projectData.js
@@ -10,13 +10,21 @@ export const ProjectData = [
   {
     id: 2,
     title:
+    "Image style transfer using TensorFlow",
+    description:
+    "Transfer the artistic style of a 'style' image to a 'content' image. In this project, I utilize a pretrained image layer filtering algorithm VGG19 to transform the style of an image.",
+    link: "https://github.com/sagara92/Image_style_transfer_ML",
+  },
+  {
+    id: 3,
+    title:
       "Hangman ML Solver",
     description:
       "This project implements a N-Gram model to learn the word pattern from a dictionary to solve the Hangman Word Game. The N-Gram model is currently ~66% accurate when tested with training dictionary.",
     link: "https://github.com/sagara92/Hangman_ML/blob/main/hangman_solver_ML_NGram.ipynb",
   },
   {
-    id: 3,
+    id: 4,
     title:
       "Enhancing Periodicity Analysis Accuracy Through Phase Fold Amplitude Minimization (PFAM) Technique",
     description:
@@ -24,7 +32,7 @@ export const ProjectData = [
     link: "/PFAM_technique.pdf",
   },
   {
-    id: 4,
+    id: 5,
     title:
       "Can Blazar flares in gamma-ray LCs be explained by jet angle and geometry?",
     description: "In Preparation.",
